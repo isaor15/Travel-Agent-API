5d6c79c8c466cf7546925e8f95b6e336de7561c0
isaor15 <isabella.lamor15@gmail.com>
2026-03-10 08:16:33 -0400

    i actually didnt like the loop idea. remembered i needed a menu:/

--- Travel Agent.py
+++ Travel Agent.py
@@ -3,21 +3,28 @@ import requests
 coderun = True
 
 def usermenu():
-    usermenu = input("Welcome to the Bella Vista Travel Agency! Let's set up your plan!")
+    usermenu = input("Welcome to the Bella Vista Travel Agency! Let's set up your plan! Enter the corresponding number to begin\n" \
+    " 1. Find you dream destination\n " "2. Add a country to your trip\n " "3. Show trips\n 4. calculate budget\n 5. Save your travel plan")
+    
+    if usermenu == "1":
+        country_vis()
 
 def country_vis():
-    while coderun:
-        country = input("What country would you like to visit? (or type 'end' to quit): ").upper()
+        
+        country = input("What country would you like to visit?: ").title()
         url = f"https://restcountries.com/v3.1/name/{country}"
         response = requests.get(url)
 
-        if country == "end":
-            coderun = False
+        languages = ",".join(data["languages"].values())
+        currencies = ",".join(data["currencies"].keys())
+        timezones = ",".join(data["timezones"])
 
         if response.status_code == 200:
+
             data = response.json()[0]
             print("Ahh, I've found it. What a great country!")
             print(f"Here's some useful information about {country}")
+            
             #you gotta know this stuff
             print("This beautiful country's capital is:", data["capital"][0])
             print("This country's official name is", data["name"]["official"])
@@ -29,4 +36,8 @@ def country_vis():
         else:
             print("Oh no! We couldn't find that country :/")
 
-country_vis()
+def addvisit():
+
+
+    usermenu()
+    country_vis()
