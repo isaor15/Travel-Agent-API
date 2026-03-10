c5a2714e07ffe239ebf0091e8c77bec4a3f6b6a5
isaor15 <isabella.lamor15@gmail.com>
2026-03-09 21:34:01 -0400

    added all the useful info for the country

--- Travel Agent.py
+++ Travel Agent.py
@@ -1,7 +1,7 @@
 import requests
 
 def country_vis():
-    country = input("What country would you like to visit?: ")
+    country = input("What country would you like to visit?: ").upper()
     url = f"https://restcountries.com/v3.1/name/{country}"
     response = requests.get(url)
 
@@ -9,7 +9,14 @@ def country_vis():
         data = response.json()[0]
         print("Ahh, I've found it. What a great country!")
         print(f"Here's some useful information about {country}")
-        print("Capital:", data["capital"][0])
+        #you gotta know this stuff
+        print("This beautiful country's capital is:", data["capital"][0])
+        print("This country's official name is", data["name"]["official"])
+        print("The region this country is in the", data["region"])
+        print("Around", data["population"], "people live here!")
+        print("If you wanna buy anything, you'd use", data["currency"])
+        print("You better start learning", data["languages"],"!")
+        print("I sure hope you don't get jet-lagged. here the time zone is", data["timezones"])
     else:
         print("Oh no! We couldn't find that country :/")
 
